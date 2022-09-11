47837dde97403419a6bb7657ebcbbfc82f42fa97
Theo Pearson-Bray <tpb1908dev@gmail.com>
2022-09-11 23:08:02 +0100

    Disable preview

--- src/App.jsx
+++ src/App.jsx
@@ -58,12 +58,6 @@ let treeData;
 function App() {
     const [doc, setDoc] = useState("");
     const [theme, setTheme] = useState(getTheme("githubDark"))
-    const [editorRef, editorView, setConfig] = useCodemirror({
-        initialDoc: doc,
-        setDoc,
-        plugins: [syncPlugin],
-        live: {theme}
-    });
     const mouseIsOn = useRef(null);
 
     const defaultPlugin = () => (tree) => {
@@ -74,100 +68,100 @@ function App() {
     const markdownElem = document.getElementById("markdown");
     const previewElem = document.getElementById("preview");
 
-    const computeElemsOffsetTop = () => {
-        let markdownChildNodesOffsetTopList = [];
-        let previewChildNodesOffsetTopList = [];
-
-        treeData.children.forEach((child, index) => {
-            if (child.type !== "element" || child.position === undefined) return;
-
-            const pos = child.position.start.offset;
-            const lineInfo = editorView.lineBlockAt(pos);
-            const offsetTop = lineInfo.top;
-            markdownChildNodesOffsetTopList.push(offsetTop);
-            previewChildNodesOffsetTopList.push(
-                previewElem.childNodes[index].offsetTop -
-                previewElem.getBoundingClientRect().top //offsetTop from the top of preview
-            );
-        });
-
-        return [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList];
-    };
-    const handleMdScroll = () => {
-        console.log(mouseIsOn.current);
-        if (mouseIsOn.current !== "markdown") {
-            return;
-        }
-        const [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList] =
-            computeElemsOffsetTop();
-        let scrollElemIndex;
-        for (let i = 0; markdownChildNodesOffsetTopList.length > i; i++) {
-            if (markdownElem.scrollTop < markdownChildNodesOffsetTopList[i]) {
-                scrollElemIndex = i - 1;
-                break;
-            }
-        }
-
-        if (
-            markdownElem.scrollTop >=
-            markdownElem.scrollHeight - markdownElem.clientHeight //true when scroll reached the bottom
-        ) {
-            previewElem.scrollTop =
-                previewElem.scrollHeight - previewElem.clientHeight; //scroll to the bottom
-            return;
-        }
-
-        if (scrollElemIndex >= 0) {
-            let ratio =
-                (markdownElem.scrollTop -
-                    markdownChildNodesOffsetTopList[scrollElemIndex]) /
-                (markdownChildNodesOffsetTopList[scrollElemIndex + 1] -
-                    markdownChildNodesOffsetTopList[scrollElemIndex]);
-            previewElem.scrollTop =
-                ratio *
-                (previewChildNodesOffsetTopList[scrollElemIndex + 1] -
-                    previewChildNodesOffsetTopList[scrollElemIndex]) +
-                previewChildNodesOffsetTopList[scrollElemIndex];
-        }
-    };
-
-    const handlePreviewScroll = () => {
-        if (mouseIsOn.current !== "preview") {
-            return;
-        }
-        const [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList] =
-            computeElemsOffsetTop();
-        let scrollElemIndex;
-        for (let i = 0; previewChildNodesOffsetTopList.length > i; i++) {
-            if (previewElem.scrollTop < previewChildNodesOffsetTopList[i]) {
-                scrollElemIndex = i - 1;
-                break;
-            }
-        }
-
-        if (scrollElemIndex >= 0) {
-            let ratio =
-                (previewElem.scrollTop -
-                    previewChildNodesOffsetTopList[scrollElemIndex]) /
-                (previewChildNodesOffsetTopList[scrollElemIndex + 1] -
-                    previewChildNodesOffsetTopList[scrollElemIndex]);
-            markdownElem.scrollTop =
-                ratio *
-                (markdownChildNodesOffsetTopList[scrollElemIndex + 1] -
-                    markdownChildNodesOffsetTopList[scrollElemIndex]) +
-                markdownChildNodesOffsetTopList[scrollElemIndex];
-        }
-    };
-
-    const md = unified()
-        .use(remarkParse)
-        .use(remarkGfm)
-        .use(remarkMath)
-        .use(remarkRehype)
-        .use(defaultPlugin)
-        .use(rehypeMathJaxSvg)
-        .use(rehypeReact, {createElement, Fragment})
-        .processSync(doc).result;
+    // const computeElemsOffsetTop = () => {
+    //     let markdownChildNodesOffsetTopList = [];
+    //     let previewChildNodesOffsetTopList = [];
+    //
+    //     treeData.children.forEach((child, index) => {
+    //         if (child.type !== "element" || child.position === undefined) return;
+    //
+    //         const pos = child.position.start.offset;
+    //         //const lineInfo = editorView.lineBlockAt(pos);
+    //         const offsetTop = lineInfo.top;
+    //         markdownChildNodesOffsetTopList.push(offsetTop);
+    //         previewChildNodesOffsetTopList.push(
+    //             previewElem.childNodes[index].offsetTop -
+    //             previewElem.getBoundingClientRect().top //offsetTop from the top of preview
+    //         );
+    //     });
+    //
+    //     return [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList];
+    // };
+    // const handleMdScroll = () => {
+    //     console.log(mouseIsOn.current);
+    //     if (mouseIsOn.current !== "markdown") {
+    //         return;
+    //     }
+    //     const [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList] =
+    //         computeElemsOffsetTop();
+    //     let scrollElemIndex;
+    //     for (let i = 0; markdownChildNodesOffsetTopList.length > i; i++) {
+    //         if (markdownElem.scrollTop < markdownChildNodesOffsetTopList[i]) {
+    //             scrollElemIndex = i - 1;
+    //             break;
+    //         }
+    //     }
+    //
+    //     if (
+    //         markdownElem.scrollTop >=
+    //         markdownElem.scrollHeight - markdownElem.clientHeight //true when scroll reached the bottom
+    //     ) {
+    //         previewElem.scrollTop =
+    //             previewElem.scrollHeight - previewElem.clientHeight; //scroll to the bottom
+    //         return;
+    //     }
+    //
+    //     if (scrollElemIndex >= 0) {
+    //         let ratio =
+    //             (markdownElem.scrollTop -
+    //                 markdownChildNodesOffsetTopList[scrollElemIndex]) /
+    //             (markdownChildNodesOffsetTopList[scrollElemIndex + 1] -
+    //                 markdownChildNodesOffsetTopList[scrollElemIndex]);
+    //         previewElem.scrollTop =
+    //             ratio *
+    //             (previewChildNodesOffsetTopList[scrollElemIndex + 1] -
+    //                 previewChildNodesOffsetTopList[scrollElemIndex]) +
+    //             previewChildNodesOffsetTopList[scrollElemIndex];
+    //     }
+    // };
+    //
+    // const handlePreviewScroll = () => {
+    //     if (mouseIsOn.current !== "preview") {
+    //         return;
+    //     }
+    //     const [markdownChildNodesOffsetTopList, previewChildNodesOffsetTopList] =
+    //         computeElemsOffsetTop();
+    //     let scrollElemIndex;
+    //     for (let i = 0; previewChildNodesOffsetTopList.length > i; i++) {
+    //         if (previewElem.scrollTop < previewChildNodesOffsetTopList[i]) {
+    //             scrollElemIndex = i - 1;
+    //             break;
+    //         }
+    //     }
+    //
+    //     if (scrollElemIndex >= 0) {
+    //         let ratio =
+    //             (previewElem.scrollTop -
+    //                 previewChildNodesOffsetTopList[scrollElemIndex]) /
+    //             (previewChildNodesOffsetTopList[scrollElemIndex + 1] -
+    //                 previewChildNodesOffsetTopList[scrollElemIndex]);
+    //         markdownElem.scrollTop =
+    //             ratio *
+    //             (markdownChildNodesOffsetTopList[scrollElemIndex + 1] -
+    //                 markdownChildNodesOffsetTopList[scrollElemIndex]) +
+    //             markdownChildNodesOffsetTopList[scrollElemIndex];
+    //     }
+    // };
+    //
+    // const md = unified()
+    //     .use(remarkParse)
+    //     .use(remarkGfm)
+    //     .use(remarkMath)
+    //     .use(remarkRehype)
+    //     .use(defaultPlugin)
+    //     .use(rehypeMathJaxSvg)
+    //     .use(rehypeReact, {createElement, Fragment})
+    //     .processSync(doc).result;
 
     function changeTheme(value) {
         console.log("Changing theme");
@@ -188,16 +182,16 @@ function App() {
             ]
             }/>
             //
-            <div id="editor-wrapper">
+            <div id="editor-wrapper" >
 
                 <ReactCodeMirror extensions={extensions} theme={theme}/>
                 <div
                     id="preview"
                     className="markdown-body"
-                    onScroll={handlePreviewScroll}
+                    // onScroll={handlePreviewScroll}
                     onMouseEnter={() => (mouseIsOn.current = "preview")}
                 >
-                    {md}
+                    {/*{md}*/}
                 </div>
             </div>
 
